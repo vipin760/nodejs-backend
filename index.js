f833abba34c41b941e91c0016e1a42a75889596c
vipinm500@gmail.com <vipin.m@astriadigital.com>
2024-11-25 22:12:17 +0530

    added .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send({message:"working fine......."});
+    res.send({message:"working fine.......!"});
 })
 
 module.exports = app
